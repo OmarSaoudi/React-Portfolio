30ba0f4049e70ca9d930e080503a6771d3b88430
Omar Saoudi <saoudiomar518@gmail.com>
2023-05-07 01:33:33 +0200

    react-project

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react'
+import '../../asset/css/Bootstrap.min.css'
+import '../../asset/css/custom.css'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+
+export class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment>
+
+        <Container fluid={true} className="topFixedBanner p-0">
+          <div className="topBannerOverly">
+            <Container className="topContent"  >
+              <Row>
+                <Col className="text-center"  >
+                  <h1  className="topTitle" >  Creative Team Programming  </h1>
+                  <h4 className="topSubTitle">  Learn Programing skills </h4>
+                  <Button variant="primary">More</Button>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default TopBanner
